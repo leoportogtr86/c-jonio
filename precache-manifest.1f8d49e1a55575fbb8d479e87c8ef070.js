585b5ed2b7865d98c8c7c8e5b8c2c562d438ccf0
Leo Porto <leonardoreisporto@gmail.com>
2020-06-15 07:18:09 -0300

    Updates

---
+++ precache-manifest.1f8d49e1a55575fbb8d479e87c8ef070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09ba8a66445cf0305af250e1654ede6",
+    "url": "/c-jonio/index.html"
+  },
+  {
+    "revision": "1e5aa56d67fbbf8cd3f3",
+    "url": "/c-jonio/static/css/main.6db28dbe.chunk.css"
+  },
+  {
+    "revision": "fbe05bdb81e5662ec041",
+    "url": "/c-jonio/static/js/2.dc5b70c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/c-jonio/static/js/2.dc5b70c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5aa56d67fbbf8cd3f3",
+    "url": "/c-jonio/static/js/main.4aa7d091.chunk.js"
+  },
+  {
+    "revision": "38769917ffefcf5dbd54",
+    "url": "/c-jonio/static/js/runtime-main.944583d6.js"
+  }
+]);
